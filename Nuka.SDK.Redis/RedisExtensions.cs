9b54e6d2738713ae6420e71f989ea97cd915a66b
Xiang Zhu <zhuxiang1212@hotmail.com>
2020-10-03 21:07:04 +0800

    [FEAT]: Finish Redis SDK

---
+++ Nuka.SDK.Redis/RedisExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace Nuka.SDK.Redis
+{
+    [ExcludeFromCodeCoverage]
+    public static class RedisExtensions
+    {
+        private const string HmGetScript = "return redis.call('HMGET', KEY[1], unpack(ARGV))";
+
+        internal static RedisValue[] HashMemberGet(
+            this IDatabase database,
+            string key,
+            params string[] members)
+        {
+            return (RedisValue[]) database.ScriptEvaluate(
+                HmGetScript,
+                new RedisKey[] {key},
+                GetRedisMembers(members));
+        }
+
+        internal static async Task<RedisValue[]> HashMemberGetAsync(
+            this IDatabase database,
+            string key,
+            params string[] members)
+        {
+            return (RedisValue[]) await database.ScriptEvaluateAsync(
+                HmGetScript,
+                new RedisKey[] {key},
+                GetRedisMembers(members));
+        }
+
+        private static RedisValue[] GetRedisMembers(params string[] members)
+        {
+            return members.Select(member => (RedisValue) member).ToArray();
+        }
+    }
+}
---
+++ Nuka.SDK.Redis/ServiceCollectionExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace Nuka.SDK.Redis
+{
+    [ExcludeFromCodeCoverage]
+    public static class ServiceCollectionExtensions
+    {
+        private const int DefaultConnectTimeoutMs = 30000;
+        private const int RetryBackoffMs = 500;
+
+        public static IServiceCollection AddNukeRedisCache(
+            this IServiceCollection serviceCollection,
+            IConfiguration configuration)
+        {
+            var redisConfig = configuration.GetSection("Redis");
+            var connectionString = redisConfig["ConnectionString"];
+            var connectTimeout = GetConnectionTimeout(redisConfig["ConnectTimeout"]);
+            var requestTimeout = GetConnectionTimeout(redisConfig["RequestTimeout"]);
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                serviceCollection.AddDistributedMemoryCache();
+            }
+            else
+            {
+                serviceCollection.Configure<RedisCacheOptions>(options =>
+                {
+                    options.ConfigurationOptions = ConfigurationOptions.Parse(connectionString);
+                    options.ConfigurationOptions.ReconnectRetryPolicy =
+                        new ExponentialRetry(RetryBackoffMs, connectTimeout);
+                    options.ConfigurationOptions.ConnectTimeout = connectTimeout;
+                    options.ConfigurationOptions.AsyncTimeout = requestTimeout;
+                });
+
+                serviceCollection.AddSingleton<IDistributedCache, NukaRedisCache>();
+            }
+
+            return serviceCollection;
+        }
+
+        private static int GetConnectionTimeout(string connectTimeout)
+        {
+            if (string.IsNullOrWhiteSpace(connectTimeout))
+            {
+                return DefaultConnectTimeoutMs;
+            }
+
+            if (!int.TryParse(connectTimeout, out var connectTimeoutMs))
+            {
+                throw new ArgumentOutOfRangeException(nameof(connectTimeout), connectTimeout,
+                    "The value provided for Redis:ConnectTimeout is not a valid integer.");
+            }
+
+            return connectTimeoutMs;
+        }
+    }
+}
